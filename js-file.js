a443770c69ae1e473d5de465e9a1f4cc13c253e6
Unstable Groot <115673057+UnstableGrooot@users.noreply.github.com>
2023-07-28 05:10:09 -0400

    Title and button to create grid implimented

--- js-file.js
+++ js-file.js
@@ -1,19 +1,49 @@
+const bColor = "#dfdfdf";
+
+const body = document.querySelector("body");
+body.style.backgroundColor = bColor
+
 const container = document.querySelector(".grid-container");
 
-const size = prompt("what size grid?");
-
-for (let i = 0; i < size * size; i++){
-    const block = document.createElement("div");
-    block.classList.add("block");
-    block.style.width = 100.0 / size + "%"
-    block.addEventListener("mouseover", fadein);
-    block.addEventListener("mouseout", fadeout);
-    container.appendChild(block);
-}
+const resizeBtn = document.querySelector(".btn-zone button");
+resizeBtn.addEventListener("click", createGrid);
 
 function fadein() {
-    this.classList.add("test");
+    this.style.backgroundColor = "#000000";
+    this.style.transition = "all 0s";
 }
 function fadeout() {
-    this.classList.remove("test");
+    this.style.backgroundColor = bColor;
+    this.style.transition = "all 1.5s";
 }
+
+function createGrid() {
+    let size = prompt("what size grid?");
+
+    //intput check and limit size
+    if (size == null || size == "" || isNaN(1+size)) return;
+    if (size > 100) size = 100;
+    
+    container.style.border = "7px solid black" //make grid visable
+    resizeBtn.textContent = "Click again to create new Grid"
+    //clear old grid
+    container.innerHTML = "";
+    
+    //create grid
+    for (let i = 0; i < size * size; i++){
+        const block = document.createElement("div");
+        block.classList.add("block");
+        block.style.width = 100.0 / size + "%"
+        block.style.aspectRatio = 1;
+        block.addEventListener("mouseover", fadein);
+        block.addEventListener("mouseout", fadeout);
+        container.appendChild(block);
+    }
+
+    return
+}
+
+
+
+
+
