4ad09d2695ed797d69c6d1f9bfb0969c0ec330b6
Unstable Groot <115673057+UnstableGrooot@users.noreply.github.com>
2023-07-28 03:46:34 -0400

    Add fade effect when mouse over grid element

--- js-file.js
+++ js-file.js
@@ -6,5 +6,14 @@ for (let i = 0; i < size * size; i++){
     const block = document.createElement("div");
     block.classList.add("block");
     block.style.width = 100.0 / size + "%"
+    block.addEventListener("mouseover", fadein);
+    block.addEventListener("mouseout", fadeout);
     container.appendChild(block);
+}
+
+function fadein() {
+    this.classList.add("test");
+}
+function fadeout() {
+    this.classList.remove("test");
 }
